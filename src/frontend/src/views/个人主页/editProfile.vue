52320fd79e7e7db35094de73ea31e9d9d07f5036
jackie56678 <jack666ie@gmail.com>
2024-05-16 21:26:47 +0800

    update

--- src/frontend/src/views/个人主页/editProfile.vue
+++ src/frontend/src/views/个人主页/editProfile.vue
@@ -3,20 +3,20 @@
     <div slot="header" class="clearfix">
       <span>编辑个人信息</span>
     </div>
-    <el-form :model="userForm" label-position="top" label-width="80px">
-      <el-form-item label="Name">
+    <el-form :model="userForm" label-position="left" label-width="80px">
+      <el-form-item label="姓名">
         <el-input v-model="userForm.name"></el-input>
       </el-form-item>
-      <el-form-item label="Gender">
+      <el-form-item label="性别">
         <el-radio-group v-model="userForm.gender">
           <el-radio label="男">男</el-radio>
           <el-radio label="女">女</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="Birth Date">
+      <el-form-item label="出生日期">
         <el-date-picker v-model="userForm.birthDate" type="date" placeholder="选择日期"></el-date-picker>
       </el-form-item>
-      <el-form-item label="description">
+      <el-form-item label="个人描述">
         <el-input v-model="userForm.description"></el-input>
       </el-form-item>
     </el-form>
