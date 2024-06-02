727684a5d92b6b550b958bda83952fd9269df06f
jackie56678 <jack666ie@gmail.com>
2024-06-02 23:07:26 +0800

    update

--- src/sql/chat.sql
+++ src/sql/chat.sql
@@ -1,15 +1,9 @@
--- chat页面
 CREATE TABLE chat (
-		cid INT AUTO_INCREMENT PRIMARY KEY,
+	cid INT AUTO_INCREMENT PRIMARY KEY,
     uid INT,
-		sessionid INT,
-		`date` Date,
+	sessionid INT,
+	`date` Date,
     question VARCHAR(255) NOT NULL,
-		answer VARCHAR(255) NOT NULL
+	answer VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE chathistory (
-    uid INT,
-		sessionid INT AUTO_INCREMENT PRIMARY KEY,
-		`date` Date
-);
--- src/sql/source.sql
+++
@@ -1,7 +0,0 @@
--- 资源表：SID（source ID，primary key）,ID(foreign id), type, URL
-CREATE TABLE source(
-    sid INT PRIMARY KEY AUTO_INCREMENT,
-    id INT,
-    type VARCHAR(100),
-    url VARCHAR(255)
-);
